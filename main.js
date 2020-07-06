2d38e7977e95c53723e2d34ecf01c3fa937cdd35
Chanho Park <chanho9.park@samsung.com>
2020-07-06 18:14:19 +0900

    add files

---
+++ main.js
@@ -0,0 +1,14 @@
+function sendLoginAction() {
+	try {
+	    webkit.messageHandlers.loginAction.postMessage(
+	    	document.getElementById("email").value + " " + document.getElementById("password").value
+	    );
+    } catch(err) {
+        console.log('The native context does not exist yet');
+    }
+}
+
+function mobileHeader() {
+    document.querySelector('h1').innerHTML = "WKWebView Mobile";
+		document.querySelector('h1').color = 'red';
+}
