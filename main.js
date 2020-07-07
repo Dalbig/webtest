6290df6df425507ac3064500d9a06332acacf233
Chanho Park <chanho9.park@samsung.com>
2020-07-07 19:52:10 +0900

    test10

--- main.js
+++ main.js
@@ -39,8 +39,7 @@ function launchPhotoGallery() {
 }
 
 window.getDataFromNative = function(data) {
-    document.body.style.backgroundColor = "red";
-
+    
     window.callbackForNative(data)
 }
 
@@ -50,6 +49,7 @@ window.callbackForNative = function(data) {}
 // Somewhere in your code where you want to send data to the native app and have it call a JS callback with some data:
 window.callbackForNative = function(data) {
     // Do your stuff here with the data returned from the native app
+    document.body.style.backgroundColor = "red";
 } 
 
 // window.callbackForNative = function(data) { }
