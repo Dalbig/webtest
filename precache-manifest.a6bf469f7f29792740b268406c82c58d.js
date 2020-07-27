519ac5c95e66e7ff9d5e6dba0086eff04bde162a
Chanho Park <chanho9.park@samsung.com>
2020-07-27 19:56:29 +0900

    add test

---
+++ precache-manifest.a6bf469f7f29792740b268406c82c58d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a1f5cbd5ff30e14eb17c53c54001a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ed1a88452fca2f8f99d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0d4be64a66fc92aa4a21",
+    "url": "/static/js/2.2dd149f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2dd149f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed1a88452fca2f8f99d",
+    "url": "/static/js/main.7451fe91.chunk.js"
+  },
+  {
+    "revision": "2f78652e1dd1c232bae4",
+    "url": "/static/js/runtime-main.95e34809.js"
+  }
+]);
